8efcb9e2a6fef7b6f2bb0f1c27d2821e50f1905b
Andres Perez <aperez@salesforce.com>
2022-06-18 13:47:33 -0400

    Make it a -- **Web service** --

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const path = require('path');
 const logger = require('morgan');
 const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
 app.use('/quotes', require('./routes/quotes'));
 
 // catch 404 and forward to error handler
@@ -29,10 +22,8 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err);
 });
 
 module.exports = app;
--- routes/quotes.js
+++ routes/quotes.js
@@ -6,14 +6,7 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
     try {
         const rows = await db.query('SELECT id, quote, author FROM quote');
-        // res.json(rows);
-        res.render('quotes', {
-            title: "Quotes",
-            rows,
-            meta: {
-                page: rows.length
-            }
-        });
+        res.json(rows);
     } catch (err) {
         console.error(`Error while getting quotes `, err.message);
         next(err);
@@ -23,13 +16,11 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
     debugger;
     const { quote, author } = req.body;
-    const result = await db.query('INSERT INTO quote(quote, author) VALUES ($1, $2) RETURNING *', [quote, author]);
-
-    if (!result.length) {
+    try {
+        const result = await db.query('INSERT INTO quote(quote, author) VALUES ($1, $2) RETURNING *', [quote, author]);
         res.json(result);
-        return;
-    } else {
-        res.redirect("/quotes");
+    } catch (err) {
+        next(err.detail ? err.detail : err);
     }
 });
 
