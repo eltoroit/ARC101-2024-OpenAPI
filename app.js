35129cd949cb28a19e1ba7277f2dab5ee3d0e5d0
Andres Perez <aperez@salesforce.com>
2022-06-18 10:04:43 -0400

    Added quotes from tutorial

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-const createError = require('http-errors');
-const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const express = require('express');
 const app = express();
 
 // view engine setup
@@ -19,16 +15,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+app.use('/quotes', require('./routes/quotes'));
 
 // catch 404 and forward to error handler
-app.use( (req, res, next) => {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use( (err, req, res, next) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
