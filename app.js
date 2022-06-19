70553aea69de7614271a1a7d23490b6bd932cb58
Andres Perez <aperez@salesforce.com>
2022-06-18 20:15:37 -0400

    Create a JSON schema renderer

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const swaggerUI = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
 const app = express();
 
 app.use(logger('dev'));
@@ -11,27 +10,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const swaggerJsDoc = require("swagger-jsdoc")({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Quotes API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    },
+    servers: [
+      { url: "http://localhost:3000" },
+      { url: "https://et-demo-arc101-002.herokuapp.com" },
+    ],
+  },
+  apis: ["./routes/*.js"],
+});
+
 app.use('/', require('./routes/index'));
 app.use('/quotes', require('./routes/quotes'));
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(
-  {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Quotes API",
-        version: "1.0.0",
-        description: "A simple Express Library API",
-      },
-      servers: [
-        { url: "http://localhost:3000" },
-        { url: "https://et-demo-arc101-002.herokuapp.com" },
-      ],
-    },
-    apis: ["./routes/*.js"],
-  })
-));
-
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
+app.get('/api-docs.json', (req, res, next) => {
+  res.json(swaggerJsDoc);
+})
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
