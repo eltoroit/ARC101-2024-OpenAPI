c0e5a4d74ff2bd539656a35d0e60fb65022b5dc2
@ELTOROIT - Andres Perez <andresperezf@yahoo.com>
2024-09-10 13:53:02 -0400

    Cleaned Games

--- public/pke/script.js
+++ public/pke/script.js
@@ -1,6 +1,7 @@
 import { BigNumber } from "https://cdn.jsdelivr.net/npm/bignumber.js@9.1.2/bignumber.min.mjs";
 export default class InputHandler {
 	values = {};
+	hideSecrets = false;
 
 	constructor() {
 		this.values = {
@@ -9,22 +10,18 @@ export default class InputHandler {
 		};
 	}
 
-	changedInput(name) {
+	validateInput(name) {
 		const input = document.getElementById(name);
 		const div = document.getElementById(`${name}.div`);
 		const error = document.getElementById(`${name}.error`);
 		const number = Number(input.value);
 
-		if (Number(name[1]) === 1) {
-			document.getElementById("g1Encrypt").disabled = true;
-			document.getElementById("g1Decrypt").disabled = true;
-		} else {
-			document.getElementById("g2GenerateP1_PN").disabled = true;
-			document.getElementById("g2GenerateP2_PN").disabled = true;
-			document.getElementById("g2GenerateP1Secret").disabled = true;
-			document.getElementById("g2GenerateP2Secret").disabled = true;
-		}
+		// Disable all buttons
+		Array.from(document.getElementsByTagName("button")).forEach((button) => {
+			button.disabled = true;
+		});
 
+		// Validate Data
 		if (input.validity.valueMissing) {
 			error.textContent = "This field is required.";
 			input.classList.add("invalid");
@@ -39,82 +36,124 @@ export default class InputHandler {
 			return;
 		}
 
+		// Clear errors
 		error.textContent = "";
 		input.classList.remove("invalid");
 		div.classList.remove("slds-has-error");
 		this.values[name] = number;
 
+		// Enable button
+		this.enableButton(name);
+	}
+
+	enableButton(name) {
+		const isValid = () => {
+			const input = document.getElementById(name);
+			if (input.validity.valueMissing) return false;
+			if (input.validity.rangeUnderflow || input.validity.rangeOverflow) return false;
+			return true;
+		};
+
+		// Disable all buttons
+		Array.from(document.getElementsByTagName("button")).forEach((button) => {
+			button.disabled = true;
+		});
+
+		// Enable corresponding button
 		switch (name) {
 			case "g1GuardedValue":
-				document.getElementById("g1Encrypt").disabled = false;
+				if (isValid()) {
+					document.getElementById("g1Encrypt").disabled = false;
+				}
 				break;
 			case "g1EncryptedValue":
-				document.getElementById("g1Decrypt").disabled = false;
+				if (isValid()) {
+					document.getElementById("g1Decrypt").disabled = false;
+				}
 				break;
 			case "g2P1_PRIVATE":
-				document.getElementById("g2GenerateP1_PN").disabled = false;
+				if (isValid()) {
+					document.getElementById("g2GenerateP1_PN").disabled = false;
+				}
 				break;
 			case "g2P2_PRIVATE":
-				document.getElementById("g2GenerateP2_PN").disabled = false;
+				if (isValid()) {
+					document.getElementById("g2GenerateP2_PN").disabled = false;
+				}
 				break;
 			case "g2P1_P2_PN":
-				document.getElementById("g2GenerateP1Secret").disabled = false;
+				if (isValid()) {
+					document.getElementById("g2GenerateP1Secret").disabled = false;
+				}
 				break;
 			case "g2P2_P1_PN":
-				document.getElementById("g2GenerateP2Secret").disabled = false;
+				if (isValid()) {
+					document.getElementById("g2GenerateP2Secret").disabled = false;
+				}
 				break;
 			default:
 				break;
 		}
 	}
 
-	#delay = 100;
+	initialize() {
+		// Inputs
+		Array.from(document.getElementsByTagName("input"))
+			.filter((input) => input.type === "number" && !input.disabled)
+			.forEach((input) => {
+				input.addEventListener("input", (element) => {
+					this.validateInput(element.target.id);
+				});
+				input.addEventListener("click", (element) => {
+					this.enableButton(element.target.id);
+				});
+			});
+
+		// Buttons
+		Array.from(document.getElementsByTagName("button")).forEach((button) => {
+			console.log(button.id);
+			button.addEventListener("click", (element) => {
+				this[element.target.id]();
+			});
+		});
+
+		// Toggle
+		document.getElementById("showSecrets").addEventListener("click", this.toggleSecrets.bind(this));
+	}
+
+	toggleSecrets() {
+		this.hideSecrets = !this.hideSecrets;
+		this.#showHideSecrets();
+	}
+
 	g1Encrypt() {
-		const encrypted = this.values.g1GuardedValue * this.values.g1SharedKey;
-		const msg = `Please share this value: ${encrypted.toLocaleString()}`;
-		document.getElementById("g1EncryptedResult").value = msg;
-		setTimeout(() => {
-			alert(msg);
-		}, this.#delay);
+		const encrypted = (this.values.g1GuardedValue * this.values.g1P1SharedKey).toLocaleString();
+		document.getElementById("g1EncryptedResult").value = encrypted;
 	}
 
 	g1Decrypt() {
-		const decrypted = this.values.g1EncryptedValue / this.values.g1SharedKey;
-		const msg = `Please share this value: ${decrypted.toLocaleString()}`;
-		document.getElementById("g1GuardedValueResult").value = msg;
-		setTimeout(() => {
-			alert(msg);
-		}, this.#delay);
+		const decrypted = (this.values.g1EncryptedValue / this.values.g1P2SharedKey).toLocaleString();
+		document.getElementById("g1GuardedValueResult").value = decrypted;
 	}
 
 	g2GenerateP1_PN() {
-		const P1_PN = this.#expMod({ base: this.values.g2Generator, privKey: this.values.g2P1_PRIVATE }); // g^P1_PRIVATE mod p
-		const msg = `Please share this value: ${P1_PN.toLocaleString()}`;
-		document.getElementById("g2P1_PN").value = msg;
-		setTimeout(() => {
-			alert(msg);
-		}, this.#delay);
+		const P1_PN = this.#expMod({ base: this.values.g2Generator, privKey: this.values.g2P1_PRIVATE }).toLocaleString(); // g^P1_PRIVATE mod p
+		document.getElementById("g2P1_PN").value = P1_PN;
 	}
 
 	g2GenerateP2_PN() {
-		const P2_PN = this.#expMod({ base: this.values.g2Generator, privKey: this.values.g2P2_PRIVATE }); // g^P2_PRIVATE mod p
-		const msg = `Please share this value: ${P2_PN.toLocaleString()}`;
-		document.getElementById("g2P2_PN").value = msg;
-		setTimeout(() => {
-			alert(msg);
-		}, this.#delay);
+		const P2_PN = this.#expMod({ base: this.values.g2Generator, privKey: this.values.g2P2_PRIVATE }).toLocaleString(); // g^P2_PRIVATE mod p
+		document.getElementById("g2P2_PN").value = P2_PN;
 	}
 
 	g2GenerateP1Secret() {
-		const p1Secret = this.#expMod({ base: this.values.g2P1_P2_PN, privKey: this.values.g2P1_PRIVATE });
-		const msg = `SECRET: ${p1Secret.toLocaleString()}`;
-		document.getElementById("g2P1Secret").value = msg;
+		const p1Secret = this.#expMod({ base: this.values.g2P1_P2_PN, privKey: this.values.g2P1_PRIVATE }).toLocaleString();
+		document.getElementById("g2P1Secret").value = p1Secret;
 	}
 
 	g2GenerateP2Secret() {
-		const p2Secret = this.#expMod({ base: this.values.g2P2_P1_PN, privKey: this.values.g2P2_PRIVATE });
-		const msg = `SECRET: ${p2Secret.toLocaleString()}`;
-		document.getElementById("g2P2Secret").value = msg;
+		const p2Secret = this.#expMod({ base: this.values.g2P2_P1_PN, privKey: this.values.g2P2_PRIVATE }).toLocaleString();
+		document.getElementById("g2P2Secret").value = p2Secret;
 	}
 
 	#expMod({ base, privKey }) {
@@ -122,5 +161,13 @@ export default class InputHandler {
 		const bnPOWER = bnBase.exponentiatedBy(privKey);
 		const bnPN = bnPOWER.modulo(this.values.g2Prime); // base^PRIVATE mod p
 		return bnPN.toNumber();
+	}
+
+	#showHideSecrets() {
+		document.querySelectorAll(`[data-input="secret"]`).forEach((input) => {
+			input.classList[this.hideSecrets ? "remove" : "add"]("secret");
+		});
+
+		// document.getElementById("showSecrets").checked = this.hideSecrets;
 	}
 }
